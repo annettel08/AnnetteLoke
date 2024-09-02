f8744b083bb4d34c8b9ee6b0f81b11af4eec6728
SilasTSL <silastaysl@gmail.com>
2024-09-02 17:13:03 +0800

    Home page init

--- src/app/page.js
+++ src/app/page.js
@@ -1,94 +1,35 @@
-import Image from "next/image";
+import { Nav } from "@/components/Nav";
 import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <Nav />
+      <div className={styles.body}>
+        <div className={styles.heroContainer}>
+          <div className={styles.heroTextContainer}>
+            <h1 className={styles.heroText}>Hello! I'm Annette, a</h1>
+            <h2 className={styles.heroText}><span className={styles.uxDesigner}>Cutie Pie</span>.</h2>
+            <p className={styles.description}>
+            Currently finishing my last year as an Economics student at the National University of Singapore. I have previously held internships at <Link className={styles.link} href={'/#govtech'} sx={{ display: 'inline' }}>GovTech</Link> and <Link className={styles.link} href={'/#sginnovate'} sx={{ display: 'inline' }}>SGInnovate</Link> - do check my work out!
+            </p>
+          </div>
+          <img className={styles.profilePicture} src="/profile_picture.png" />
         </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className={styles.linksContainer}>
+          <Link className={styles.linkContainerLink} href={'/govtech'}>
+            <p className={styles.linkTitle}>Internship at GovTech</p>
+            <img className={styles.linkImg} src="/govtech.jpg" />
+          </Link>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <Link className={styles.linkContainerLink} href={'/govtech'}>
+            <p className={styles.linkTitle}>Internship at GovTech</p>
+            <img className={styles.linkImg} src="/sginnovate.jpeg" />
+          </Link>
+        </div>
       </div>
     </main>
   );
---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Nav.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
+
+export const Nav = () => {
+  return (
+    <div className={styles.nav}>
+        <Link href={'/'}>
+            <Image src={'/logo.png'} width={57} height={50} alt="logo" priority />
+        </Link>
+        <div className={styles.linkContainer}>
+            <Link className={styles.link} href={'/'}>About</Link>
+            <Link className={styles.link} href={'/#resume'}>Resume</Link>
+            <Link className={styles.link} href={'/contact'}>Contact</Link>
+        </div>
+    </div>
+  )
+}
