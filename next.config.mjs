9e396eb468841c940b24ba3dd1ae57bcc33691fa
SilasTSL <silastaysl@gmail.com>
2024-09-14 14:42:42 +0800

    change next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/AnnetteLoke",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
