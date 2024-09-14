3ce279f35b608f6b96ad6f8adeaebe4a99bb9572
SilasTSL <silastaysl@gmail.com>
2024-09-14 14:48:15 +0800

    Change image base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/AnnetteLoke",
+    basePath: '/AnnetteLoke', // replace with your repository name
+    assetPrefix: '/AnnetteLoke', // replace with your repository name
+    images: {
+      unoptimized: true, // This is necessary because Next.js Image Optimization won't work out of the box with GitHub Pages
+    },
     output: "export",
     reactStrictMode: true,
 };
