c38f2fc46cea866b08c65b36be4c5d85c9aff5a6
SilasTSL <silastaysl@gmail.com>
2024-09-23 21:42:48 +0800

    Fix pictures in about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -7,7 +7,7 @@ export default function page() {
     return (
     <div className={styles.main}>
         <h1 className={styles.header}>Hello, I'm Annette!</h1>
-        <img className={styles.picture} src="/profile_picture.png" />
+        <img className={styles.picture} src="/AnnetteLoke/profile_picture.png" />
         <p className={styles.text}>
         I am currently a student in Singapore, approaching graduation and eager to embark on my professional 
 journey in UI/UX design. I’m actively seeking opportunities to apply my skills and contribute to exciting 
@@ -16,17 +16,17 @@ projects.<br></br><br></br>
         </p>
         <div className={styles.socialsRow}>
             <a href="https://www.linkedin.com/in/annetteloke/">
-                <img className={styles.socialLinkImg} src="/linkedin.png" />
+                <img className={styles.socialLinkImg} src="/AnnetteLoke/linkedin.png" />
             </a>
             <Link href="mailto:annettelxh@gmail.com">
-                <img className={styles.socialLinkImg} src="/email.png" />
+                <img className={styles.socialLinkImg} src="/AnnetteLoke/email.png" />
             </Link>
         </div>
-        <img style={{ transform: 'rotate(-40deg)', right: '19%', bottom: '38%' }} className={styles.icon} src="/paw.png" />
-        <img style={{ right: '30%', bottom: '50%' }} className={styles.icon} src="/badminton.png" />
-        <img style={{ right: '73%', bottom: '42%' }} className={styles.icon} src="/croissant.png" />
-        <img style={{ right: '75%', bottom: '68%', width: 'calc(3rem + 3vw)' }} className={styles.icon} src="/tennis.png" />
-        <img style={{ transform: 'rotate(-10deg)', width: 'calc(3rem + 3vw)', right: '25%', bottom: '75%' }} className={styles.icon} src="/takoyaki.png" />
+        <img style={{ transform: 'rotate(-40deg)', right: '19%', bottom: '38%' }} className={styles.icon} src="/AnnetteLoke/paw.png" />
+        <img style={{ right: '30%', bottom: '50%' }} className={styles.icon} src="/AnnetteLoke/badminton.png" />
+        <img style={{ right: '73%', bottom: '42%' }} className={styles.icon} src="/AnnetteLoke/croissant.png" />
+        <img style={{ right: '75%', bottom: '68%', width: 'calc(3rem + 3vw)' }} className={styles.icon} src="/AnnetteLoke/tennis.png" />
+        <img style={{ transform: 'rotate(-10deg)', width: 'calc(3rem + 3vw)', right: '25%', bottom: '75%' }} className={styles.icon} src="/AnnetteLoke/takoyaki.png" />
     </div>
   )
 }
